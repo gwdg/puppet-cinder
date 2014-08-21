openstack/puppet-ceilometer

Use 2.2 as the default mysql_module
Change-Id: I91c87fa0b4d5871aa936a5b4842b47f4cfee4ca8

manifests/db/mysql/host_access.pp
@@ -13,7 +13,7 @@
 #  [*database*]
 #    the database name
 #
-define ceilometer::db::mysql::host_access ($user, $password, $database, $mysql_module='0.9')  {
+define ceilometer::db::mysql::host_access ($user, $password, $database, $mysql_module='2.2')  {
   # New types for mysql module v2.0+
   if ($mysql_module >= 2.2) {
     mysql_user { "${user}@${name}":

gwdg/puppet-cinder

Change-Id: Ie93a5bee25044004b5f664038fc6961a4a8b4feb

manifests/db/mysql/host_access.pp
@@ -1,7 +1,7 @@
 #
 # Used to grant access to the cinder mysql DB
 #
-define cinder::db::mysql::host_access ($user, $password, $database, $mysql_module = '0.9')  {
+define cinder::db::mysql::host_access ($user, $password, $database, $mysql_module = '2.2')  {
   if ($mysql_module >= 2.2) {
     mysql_user { "${user}@${name}":
       password_hash => mysql_password($password),