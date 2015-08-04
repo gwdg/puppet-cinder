dac938089e865d86397e2c8cfa7d82a2710f4d52
Clayton O'Neill <clayton@oneill.net>
2015-08-04 14:53:16 -0400

    Add lock_path to cinder config
    The lock_path variable is required by cinder in Kilo and cinder-volume
    will fail to start if it does not exist.
    
    Change-Id: Ia94b17e24ec179a1804bc78c8defc25d63f7a4d3

--- manifests/init.pp
+++ manifests/init.pp
@@ -220,6 +220,11 @@
 #   (Optional) Whether to enable the v1 API (true/false).
 #   Defaults to 'true'.
 #
+# [*lock_path*]
+#   (optional) Where to store lock files. This directory must be writeable
+#   by the user executing the agent
+#   Defaults to: /var/lock/cinder
+#
 # === Deprecated Parameters
 #
 # [*mysql_module*]
@@ -278,6 +283,7 @@ class cinder (
   $default_availability_zone          = false,
   $enable_v1_api                      = true,
   $enable_v2_api                      = true,
+  $lock_path                          = '/var/lock/cinder',
   # DEPRECATED PARAMETERS
   $mysql_module                       = undef,
 ) {
@@ -524,6 +530,7 @@ class cinder (
   cinder_config {
     'DEFAULT/enable_v1_api':        value => $enable_v1_api;
     'DEFAULT/enable_v2_api':        value => $enable_v2_api;
+    'DEFAULT/lock_path':            value => $lock_path;
   }
 
 }
