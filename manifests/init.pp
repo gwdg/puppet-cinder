53c5b31cc339be5940daab7f706d1475d2b346f0
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2015-08-04 17:16:37 +0200

    Merge: Accept 'rabbit' as rpc_backend

--- manifests/init.pp
+++ manifests/init.pp
@@ -233,7 +233,7 @@ class cinder (
   $database_max_retries               = '10',
   $database_retry_interval            = '10',
   $database_max_overflow              = undef,
-  $rpc_backend                        = 'cinder.openstack.common.rpc.impl_kombu',
+  $rpc_backend                        = 'rabbit',
   $control_exchange                   = 'openstack',
   $rabbit_host                        = '127.0.0.1',
   $rabbit_port                        = 5672,
@@ -327,7 +327,7 @@ class cinder (
     require => Package['cinder'],
   }
 
-  if $rpc_backend == 'cinder.openstack.common.rpc.impl_kombu' {
+  if $rpc_backend == 'cinder.openstack.common.rpc.impl_kombu' or $rpc_backend == 'rabbit' {
 
     if ! $rabbit_password {
       fail('Please specify a rabbit_password parameter.')
@@ -387,7 +387,7 @@ class cinder (
 
   }
 
-  if $rpc_backend == 'cinder.openstack.common.rpc.impl_qpid' {
+  if $rpc_backend == 'cinder.openstack.common.rpc.impl_qpid' or $rpc_backend == 'qpid' {
 
     if ! $qpid_password {
       fail('Please specify a qpid_password parameter.')
