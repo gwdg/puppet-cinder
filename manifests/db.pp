94ec9a97da15a80b626ff2bfc02a210efad878a9
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-06-04 17:31:27 +0200

    Add 'database_slave_connection' parameter

--- manifests/db.pp
+++ manifests/db.pp
@@ -39,14 +39,15 @@
 #   (Optional) Defaults to $::os_service_default
 #
 class cinder::db (
-  $database_db_max_retries = $::os_service_default,
-  $database_connection     = 'sqlite:////var/lib/cinder/cinder.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
+  $database_db_max_retries      = $::os_service_default,
+  $database_connection          = 'sqlite:////var/lib/cinder/cinder.sqlite',
+  $database_idle_timeout        = $::os_service_default,
+  $database_min_pool_size       = $::os_service_default,
+  $database_max_pool_size       = $::os_service_default,
+  $database_max_retries         = $::os_service_default,
+  $database_retry_interval      = $::os_service_default,
+  $database_max_overflow        = $::os_service_default,
+  $database_slave_connection    = $::os_service_default,
 ) {
 
   include ::cinder::deps
@@ -65,13 +66,14 @@ class cinder::db (
     '^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?')
 
   oslo::db { 'cinder_config':
-    db_max_retries => $database_db_max_retries,
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
+    db_max_retries      => $database_db_max_retries,
+    connection          => $database_connection_real,
+    idle_timeout        => $database_idle_timeout_real,
+    min_pool_size       => $database_min_pool_size_real,
+    max_pool_size       => $database_max_pool_size_real,
+    max_retries         => $database_max_retries_real,
+    retry_interval      => $database_retry_interval_real,
+    max_overflow        => $database_max_overflow_real,
+    slave_connection    => $database_slave_connection,
   }
 }
